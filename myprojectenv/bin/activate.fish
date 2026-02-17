ce45c1f2dcc447bcede02db5b2008ac7d8d2cf6c
root <karanjalight>
2026-02-17 21:17:40 +0000

    making this work

--- myprojectenv/bin/activate.fish
+++ myprojectenv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/won/WON-Full-Stack/myprojectenv
+set -gx VIRTUAL_ENV /home/WON-Full-Stack/myprojectenv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
